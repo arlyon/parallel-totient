447dc62c1840182e19af50672c2b9b44fa2bb2e1
Alexander Lyon <arlyon@me.com>
2020-02-18 16:05:44 +0000

    Add debug logging

---
+++ src/openmp/logging.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <omp.h>
+
+#ifndef NDEBUG
+#define DEBUG true
+#else
+#define DEBUG false
+#endif
+
+omp_lock_t lock;
+
+#define debug_print(fmt, ...)                                                                                          \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if (DEBUG)                                                                                                     \
+        {                                                                                                              \
+            time_t t = time(NULL);                                                                                     \
+            struct tm tm = *localtime(&t);                                                                             \
+            omp_set_lock(&lock);                                                                                       \
+            fprintf(stderr, "%02d:%02d:%02d ", tm.tm_hour, tm.tm_min, tm.tm_sec);                                      \
+            fprintf(stderr, "[%s:%d] ", __FILENAME__, __LINE__);                                                       \
+            fprintf(stderr, fmt, __VA_ARGS__);                                                                         \
+            omp_unset_lock(&lock);                                                                                     \
+        }                                                                                                              \
+    } while (0)
