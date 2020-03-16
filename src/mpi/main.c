c108bb062462290385169f65575113c87c3c1ca1
Jerome Illgner <j.illgner@outlook.com>
2020-03-16 15:55:44 +0000

    fixed totient-sum MPI implementation

--- src/mpi/main.c
+++ src/mpi/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <mpi.h>
 #include "../common.h"
 
 int main(int argc, char** argv)
 {
-    if (argc != 2){
-        printf("\033[0;31mError:\033[0m Must provide exactly 2 arguments (rather than %d)\n", argc);
+    if (argc-optind != 2){
+        printf("\033[0;31mError:\033[0m Must provide exactly 2 arguments (rather than %d)\n", argc - optind);
         exit(EXIT_FAILURE);
     }
 
     // Variables
     int processors, id;
     uint32_t start, end, total;
-    const uint32_t lower = strtoul(argv[0], NULL, 10);
-    const uint32_t upper = strtoul(argv[1], NULL, 10);
+    const uint32_t lower = strtoul(argv[optind], NULL, 10);
+    const uint32_t upper = strtoul(argv[optind + 1], NULL, 10);
 
     if (upper < lower){
         if (upper <= lower)
@@ -23,12 +24,12 @@ int main(int argc, char** argv)
             return 1;
         }
     }
-    const uint32_t batchSize = (upper-lower)/processors;
 
     // MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &processors);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    const uint32_t batchSize = (upper-lower)/processors;
 
     start = id * batchSize;
     end = (id+1) * batchSize;
@@ -40,6 +41,8 @@ int main(int argc, char** argv)
     }
 
     MPI_Reduce(&sum, &total, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
     MPI_Finalize();
+
+    printf("%u\n", total);
+    exit(0);
 }
