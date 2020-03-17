ae04bee9e4d60ac22884096465e20d42ed681aab
Jerome Illgner <j.illgner@outlook.com>
2020-03-17 17:48:35 +0000

    working commit

--- src/mpi/main.c
+++ src/mpi/main.c
@@ -13,10 +13,9 @@ int main(int argc, char** argv)
 
     // Variables
     int processors, id;
-    uint32_t start, end, total;
+    uint32_t total;
     const uint32_t lower = strtoul(argv[optind], NULL, 10);
     const uint32_t upper = strtoul(argv[optind + 1], NULL, 10);
-    printf("lower: %u \t upper: %u\n", lower, upper);
 
     if (upper <= lower)
     {
@@ -28,22 +27,16 @@ int main(int argc, char** argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &processors);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    const uint32_t batchSize = (upper-lower)/processors;
-
-    start = id * batchSize + lower;
-    end = (id+1) * batchSize + lower;
-    end = (end < upper) ? end : upper + 1;
-    printf("Processor %d starts at %u and ends at %u.\n", id, start, end-1);
 
     uint32_t sum = 0;
-    for (int i = start; start < end; i++){
+    for (int i = id+lower; i <= upper; i+=processors){
         sum += euler_totient(i);
     }
 
-    printf("Processor %d has finished and reduced.\n");
     MPI_Reduce(&sum, &total, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (id == 0) {
+        printf("%u\n", total);
+    }
     MPI_Finalize();
-
-    printf("%u\n", total);
     exit(0);
 }
